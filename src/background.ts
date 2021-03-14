72f1ea863ce96c3410d5ae1630573f509cf037cf
Kyle Reblora <kbreblora@up.edu.ph>
2021-03-14 15:53:37 +0800

    Remove background script

--- src/background.ts
+++
@@ -1,2 +0,0 @@
-// This file is ran as a background script
-console.log(process.env.REACT_APP_API_KEY)
--- src/content.ts
+++
@@ -1,2 +0,0 @@
-// This file is injected as a content script
-console.log(process.env.NODE_ENV)
